1cd2d5eb7eeb7db6fcea662ac0e989f7c433d587
Awesomez <likezyp@gmail.com>
2016-08-01 15:03:45 +0800

    add Stack test

--- src/test.go
+++ src/test.go
@@ -4,14 +4,43 @@ import (
 	"fmt"
 	"./sort"
 	"./tool"
+	"./stack"
 )
 
 func main() {
-	test_sort()
+
+	/*
+	array:=[2]string{"a","b"}
+
+	sl:=array[1:]
+
+	fmt.Println(array)
+
+	array[1]="c"
+
+
+	fmt.Println(array)
+
+	fmt.Println(sl)
+	*/
+
+    test_stack()
+}
+
+func test_stack(){
+	stack := &stack.Stack{}
+	stack.Push("iOS")
+	stack.Push("Android")
+	stack.Push("WindowsPhone")
+	stack.Push("Symbian")
+	stack.Push("BalckBerry")
+	for stack.Size() > 0 {
+        fmt.Println(stack.Pop())
+	}
 }
 
 func test_sort(){
-	data :=tool.MakeTestData(200);
+	data :=tool.MakeTestData(10);
 
 	fmt.Println("init data :")
 	fmt.Println(data)
@@ -21,7 +50,8 @@ func test_sort(){
 
 	//sort.SelectionSort(data)
 	//sort.BubbleSort(data)
-	sort.InsertionSort(data)
+	//sort.InsertionSort(data)
+	sort.QuickSort(data)
 
 	fmt.Println("sorted data :")
 	fmt.Println(data)
